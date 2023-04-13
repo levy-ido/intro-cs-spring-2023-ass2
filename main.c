2e1bdc61f50ce93a4a031f2f885730d611c56f07
Ido Levy <ido.levy.n@gmail.com>
2023-04-13 19:02:11 +0300

    Finished task 5.

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #define SPACE_DOT_SUM 78
 #define INT_BITS 32
+#define ZERO_ASCII 48
+#define A_ASCII 65
 int main() {
-    char user_input;
+    char user_input, c;
+    int num, reversed_num, base, digit, invalid_string, is_A_Z, is_a_z, is_digit, is_plus, is_slash;
+    long converted;
     do {
         printf("Choose an option:\n");
         printf("0. Exit\n");
@@ -82,11 +86,10 @@ int main() {
                 break;
             case '2':
                 printf("Enter a number:\n");
-                int num;
                 scanf(" %d", &num);
                 // Get nums' bits
                 unsigned int num_bits = num;
-                int reversed_num = 0;
+                reversed_num = 0;
                 unsigned int mask = 1 << (INT_BITS - 1);
                 while (num_bits > 0) {
                     // Is rightmost bin on
@@ -102,15 +105,88 @@ int main() {
                 printf("The reversed number is %d\n", reversed_num);
                 break;
             case '3':
+                // What if the reversed number overflows?
                 printf("Enter a number:\n");
                 scanf(" %d", &num);
-
+                reversed_num = 0;
+                while (num > 0) {
+                    reversed_num = reversed_num * 10 + num % 10;
+                    num /= 10;
+                }
+                printf("The reversed number is %d\n", reversed_num);
                 break;
             case '4':
-                printf("Execute task 4\n");
+                printf("Enter base:\n");
+                scanf(" %d", &base);
+                if (base < 2 || base > 9) {
+                    printf("Invalid Base\n");
+                    break;
+                }
+                num = 0;
+                invalid_string = 0;
+                printf("Enter a number:\n");
+                scanf(" %c", &c);
+                while (c != '\n') {
+                    num *= base;
+                    digit = c - ZERO_ASCII;
+                    if (digit < 0 || digit >= base) {
+                        printf("Invalid character %c in base %d\n", c, base);
+                        invalid_string = 1;
+                        // Clean stdin
+                        scanf("%*[^\n]");
+                        break;
+                    }
+                    num += digit;
+                    scanf("%c", &c);
+                }
+                if (!invalid_string) {
+                    printf("The converted number is %d\n", num);
+                }
                 break;
             case '5':
-                printf("Execute task 5\n");
+                converted = 0;
+                invalid_string = 0;
+                printf("Enter a number in base 64:\n");
+                scanf(" %c", &c);
+                while (c != '\n') {
+                    converted *= 64;
+                    is_A_Z = c >= 'A' && c <= 'Z';
+                    is_a_z = c >= 'a' && c <= 'z';
+                    is_digit = c >= '0' && c <= '9';
+                    is_plus = c == '+';
+                    is_slash = c == '/';
+                    if (!is_A_Z && !is_a_z && !is_digit && !is_plus && !is_slash) {
+                        printf("Invalid character %c in base 64\n", c);
+                        invalid_string = 1;
+                        // Clean stdin
+                        scanf("%*[^\n]");
+                        break;
+                    }
+                    if (is_A_Z) {
+                        digit = c - A_ASCII;
+                    }
+                    if (is_a_z) {
+                        // 'a' base 64 value = 26. 'a' ascii value = 97
+                        // Add the relative position of c in 'a'-'z' to the 'a' base 64 value
+                        digit = 26 + (c - 97);
+                    }
+                    if (is_digit) {
+                        // '0' base 64 value = 52
+                        // Add the relative position of c in '0'-'9' to the '0' base 64 value
+                        digit = 52 + (c - ZERO_ASCII);
+                    }
+                    if (is_plus) {
+                        digit = 62;
+                    }
+                    if (is_slash) {
+                        digit = 63;
+                    }
+                    converted += digit;
+                    scanf("%c", &c);
+                }
+                if (!invalid_string) {
+                    printf("The converted number is %ld\n", converted);
+                }
                 break;
             case '6':
                 printf("Execute task 6\n");
